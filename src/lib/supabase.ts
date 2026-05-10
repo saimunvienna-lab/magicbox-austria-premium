636bd1c9c6e195682585020f68a26b6e27850ae5
Abu Taher Mohammad Jakaria <saimunvienna@gmail.com>
2026-05-10 02:30:04 +0100

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,1 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
 
+const SUPABASE_URL = "https://xwbdoysnnpeavqjyyphv.supabase.co";
+const SUPABASE_KEY = "sb_publishable_mVZE1pgm137VSiNzd-CI9A_8DBrs4eT";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
